3f9f94d097c648013f5527b1cd6487f7ff616574
zbananasx <timurgasakim@gmail.com>
2025-09-09 09:20:00 +0300

    first commit

---
+++ src/components/Button/Button.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+const Button = (props) => {
+    return (
+        <button {...props} className={'button ' + props.className}/>
+    )
+};
+
+export default Button
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "../button/button";
+
+const Header = () => {
+    const tg = window.Telegram.WebApp;
+    const onClose = () => {
+      tg.close()
+    return (
+        <div className={'header'}>
+        <Button onClick={onClose}>Закрыть</Button>
+        <span className={'username'}>
+            {tg.initDataUnsafe?.user?.username}
+        </span>
+        </div>
+    )
+};
+}
+export default Header;
