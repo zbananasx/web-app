1f6cbfb0bd3f2b6f6d0f0b01003578db7cebbf12
zbananasx <timurgasakim@gmail.com>
2025-09-09 09:41:41 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
-import { useTelegram } from 'src/hooks/useTelegram';
+import { useTelegram } from './hooks/useTelegram';
 function App() {
     const { onToggleButton} = useTelegram();
 
