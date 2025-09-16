6cba5621df0372048080ede5bec86d177b4eebd9
zbananasx <timurgasakim@gmail.com>
2025-09-16 06:07:45 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { useAllInfo } from './hooks/useStates';
+
+function App() {
+const {
+        score,
+        totalUpgrade1Gives,       
+        clickPerOne,   
+        upgrade1View, 
+        auto1View,      
+        auto2View,      
+        onClick,       
+        upgrade1,       
+        auto1,          
+        auto2,
+        
+  } = useAllInfo()
+  return (
+    <div className="App">
+      <div>
+        <span>
+          сила клика: {clickPerOne}
+        </span>
+      </div>
+      <div>
+        <span>
+          кликов у тебя: {score}
+        </span>
+      </div>
+      <div>
+        <span> количество первых апгрейдов: {upgrade1View} доп кликов дает {totalUpgrade1Gives}</span>
+      </div>
+      <div>
+        <span> количество первых автоматических фармилок: {auto1View}</span>
+      </div>
+      <div>
+        <span> количество вторых автоматических фармилок: {auto2View}</span>
+      </div>
+      <div>
+        <button onClick={onClick}>
+    кнопочка
+        </button>
+      </div>
+      <div>
+        <button onClick={upgrade1}>
+    первая прокачка. стоит 25 кликов, дает +1 клик
+        </button>
+      </div>
+      <div>
+        <button onClick={auto1}>
+          кнопка автоматического фарма. стоит 50 кликов, дает +1 клик в секунду
+        </button>
+        <button onClick={auto2}>
+          вторая кнопка автоматического фарма.
+          стоит 250 кликов, дает +3 клика в секунду
+        </button>
+      </div>
+    </div>
+  );
+  
+}
+export default App;
---
+++ src/hooks/useStates.jsx
@@ -0,0 +1,55 @@
+   import { useState } from "react";
+   export const useAllInfo = () => {
+   const [totalUpgrade1Gives, setTotalUpgrade1Gives] = useState(0);
+    const [score, setScore] = useState(1);
+    const [clickPerOne, setClickPerOne] = useState(1);
+    const [upgrade1View, setUpgrade1View] = useState(0);
+    const [auto1View, setAuto1View] =useState(0);
+    const [auto2View, setAuto2View] =useState(0);
+        
+    function upgrade1() {
+      if (score >= 25) {
+        setScore(score => score - 25)
+        setTotalUpgrade1Gives (totalUpgrade1Gives => totalUpgrade1Gives + 1)
+        setClickPerOne (clickPerOne => clickPerOne + 1)
+        setUpgrade1View (updgrade1View => updgrade1View + 1)
+      }
+    }
+
+    function onClick() {
+      setScore(score => score + clickPerOne)
+    }
+
+    function auto1(){
+      if (score >=50) {
+        setScore(score => score - 50)
+        setInterval(function(){
+          setScore(score => score + 1)
+        },1000)
+        setAuto1View(auto1View => auto1View + 1)
+      }
+    }
+    function auto2(){
+      if (score >=250) {
+        setScore(score => score - 250)
+        setInterval(function(){
+          setScore(score => score + 3)
+        },1000)
+        setAuto2View(auto2View => auto2View + 1)
+      }
+    }
+    return {
+        upgrade1,
+        auto1,
+        auto2,
+        onClick,
+        
+        
+        score,
+        clickPerOne,
+        totalUpgrade1Gives,
+        upgrade1View,
+        auto1View,
+        auto2View,
+    }
+   }
